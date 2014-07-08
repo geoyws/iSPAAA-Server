1a205773363c42114107e7aea0f5a7a7e11a7c6c
geoyws <geoyws@gmail.com>
2014-07-08 08:52:19 +0800

    you are my friend

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,51 @@
 var express = require('express');
 var router = express.Router();
 
+// ROUTE '/fakeapi/users/1'
+router.route('/fakeapi/users/1')
+
+  .get(function(req, res) {
+    var fakeModel = {
+      "UserDetailId": "0001",
+      "FirstName": "George",
+      "LastName": "Yong",
+      "Email": "georgeyws@ifca.com.my",
+      "Phone": "0162228603",
+      "ReportTo": "Carso Leong"
+    };
+    res.json(fakeModel);
+  });
+
+// ROUTE '/fakeapi/myactivity/date'
+router.route('/fakeapi/myactivity/date')
+
+  .get(function(req, res) {
+    var fakeModel = [
+      {
+        "Activity": "Bug Fix",
+        "AccountDescription": "SP Setia Property",
+        "Hours": "3"
+      },
+      {
+        "Activity": "Bug Fix",
+        "AccountDescription": "SP Setia Hardware",
+        "Hours": "4"
+      },
+      {
+        "Activity": "Bug Fix",
+        "AccountDescription": "SP Setia Retail",
+        "Hours": "2"
+      },
+      {
+        "Activity": "Meeting",
+        "AccountDescription": "SP Setia Property",
+        "Hours": "1"
+      }
+    ];
+    res.json(fakeModel);
+  });
+
+
 // log everytime someone uses the router
 router.use(function(req, res, next) {
   console.log('Something is using our routes!');
